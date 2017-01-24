fcb54845f354638fc4a881043effd87211ae7d4c
CrAzD <jrobichaud@outlook.com>
2017-01-24 14:16:57 -0500

    0.52
    0.52
    Changed tabs to spaces for better web reading

--- game/dota_addons/vscripts/libraries/EntityManager/Setup.lua
+++ game/dota_addons/vscripts/libraries/EntityManager/Setup.lua
@@ -2,14 +2,14 @@
 
 require('libraries/EntityManager/Manager')
 if EntityManager == nil then
-	_G.EntityManager = EntityManagerInitialization(class({}))
+    _G.EntityManager = EntityManagerInitialization(class({}))
 else
-	_G.EntityManager = EntityManagerInitialization(EntityManager)
+    _G.EntityManager = EntityManagerInitialization(EntityManager)
 end
 local manager = EntityManager
 
 -- Information about the library
-manager['version'] = 0.51
+manager['version'] = 0.52
 manager['url'] = 'https://github.com/CrAzD/DotaEntityManager'
 manager['description'] = 'This library optimizes, and scopes entities. The main way of optimization is by storing everything inside the entity upon creation. Therefor eliminating function overhead (calling funcitons a million times for no reason).'
 
@@ -20,22 +20,22 @@ manager['players'] = {}
 
 -- KV file loading and initialization
 manager['kv'] = {
-	['abilities'] = LoadKeyValues('scripts/npc/npc_abilities_custom.txt'),
-	['heroes'] = LoadKeyValues('scripts/npc/npc_heroes_custom.txt'),
-	['units'] = LoadKeyValues('scripts/npc/npc_units_custom.txt'),
-	['entities'] = {}
+    ['abilities'] = LoadKeyValues('scripts/npc/npc_abilities_custom.txt'),
+    ['heroes'] = LoadKeyValues('scripts/npc/npc_heroes_custom.txt'),
+    ['units'] = LoadKeyValues('scripts/npc/npc_units_custom.txt'),
+    ['entities'] = {}
 }
 local kvTables = {['units'] = 'units', ['heroes'] = 'heroes'}
 for _, kvTable in pairs(kvTables) do
-	for key, value in pairs(manager['kv'][kvTable]) do
-		manager['kv']['entities'][key] = value or nil
-	end
+    for key, value in pairs(manager['kv'][kvTable]) do
+        manager['kv']['entities'][key] = value or nil
+    end
 end
 
 -- Messages and info spam
 print('\n\nEntityManager:  Initialization complete...'..
-	'\n\tVersion:  '..tostring(manager['version'])..
-	'\n\tURL:  '..manager['url']..
-	'\n\tDescription:  '..manager['description']..
-	'\n'
+    '\n\tVersion:  '..tostring(manager['version'])..
+    '\n\tURL:  '..manager['url']..
+    '\n\tDescription:  '..manager['description']..
+    '\n'
 )
